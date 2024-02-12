68af1c622efc0e64362af7ece691c549ca2aa3c6
Oded Rosenblatt <odedro987@gmail.com>
2024-02-13 00:19:37 +0200

    add get_source_name func

--- src/tachiyomi/core.py
+++ src/tachiyomi/core.py
@@ -1,15 +1,18 @@
 import gzip
-from dataclasses import dataclass
 from tachiyomi.model import Backup
 
 
-@dataclass
-class TachiyomiBackup:
-    filename: str
+class SourceNotFound(Exception):
+    pass
 
-    def __gunzip(self):
-        with gzip.open(self.filename, mode="rb") as bytes:
-            return bytes.read()
 
-    def read(self):
-        return Backup().create_from_bytes(self.__gunzip())
+class TachiyomiBackup(Backup):
+    def __init__(self, filename: str):
+        with gzip.open(filename, mode="rb") as bytes:
+            self.data = Backup().create_from_bytes(bytes.read())
+
+    def get_source_name(self, source_id: int) -> str:
+        source = [s for s in self.data.sources if s.id == source_id]
+        if len(source) != 1:
+            raise SourceNotFound("source not found")
+        return source[0].name
